17f93fcf8075793c54bb2f350428d9cb738caba9
Italo Patricio <elo7-developer+italo-patricio@elo7.com>
2021-04-12 18:36:34 -0300

    adicionando input e butao para submit

--- app/src/main/java/com/example/mqttchat/MainActivity.java
+++ app/src/main/java/com/example/mqttchat/MainActivity.java
@@ -4,34 +4,98 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.mqttchat.client.MyMqttClient;
 import com.example.mqttchat.model.MessageModel;
 import com.example.mqttchat.recycleradapter.ConversationRecyclerAdapter;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.gson.Gson;
 
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Timer;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IMqttMessageListener {
+
+    MyMqttClient myMqttClient;
+    List<MessageModel> messageModelList;
+    ConversationRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        messageModelList = new ArrayList<>();
+        myMqttClient = new MyMqttClient(getApplicationContext());
         setupLayout();
-
-        new MyMqttClient(getApplicationContext());
     }
 
     private void setupLayout() {
-        List<MessageModel> messageModelList = Arrays.asList(
-                new MessageModel("xablau", "123456"),
-                new MessageModel("xablau", "123456"),
-                new MessageModel("xablau", "123456")
+        messageModelList = Arrays.asList(
+//                new MessageModel("xablau", "123456"),
+//                new MessageModel("xablau", "123456"),
+//                new MessageModel("xablau", "123456")
         );
 
         RecyclerView recyclerView = findViewById(R.id.conversation_recycler_view);
-        recyclerView.setAdapter(new ConversationRecyclerAdapter(messageModelList));
+        adapter = new ConversationRecyclerAdapter(messageModelList);
+        recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
+
+        TextInputEditText inputMessage = findViewById(R.id.text_input_message);
+        Button buttonSendMessage = findViewById(R.id.button_submit_message);
+
+        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = inputMessage.getText().toString();
+                if(message != null && !message.isEmpty()) {
+                    String json = new Gson().toJson(new MessageModel(message, "19:00"));
+                    myMqttClient.sendMessage(json);
+                } else {
+                    Toast.makeText(getApplication(), "Por favor digite uma mensagem!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        myMqttClient.connectToMqtt(new IMqttActionListener(){
+
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+                myMqttClient.subscribeToMqttChannel(MainActivity.this);
+            }
+
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+
+            }
+        });
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) {
+        new Thread(
+                () -> {
+                    try {
+                        messageModelList = new ArrayList<>();
+                        MessageModel messageModel = new Gson().fromJson(new String(message.getPayload()), MessageModel.class);
+                        messageModelList.add(messageModel);
+                        adapter.notifyDataSetChanged();
+                    }catch (Exception e) {
+                        System.out.println(e);
+                    }
+                }
+        ).run();
+
     }
 }
--- app/src/main/java/com/example/mqttchat/client/MyMqttClient.java
+++ app/src/main/java/com/example/mqttchat/client/MyMqttClient.java
@@ -3,6 +3,7 @@ package com.example.mqttchat.client;
 import android.content.Context;
 
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -12,28 +13,39 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 public class MyMqttClient {
 
     private final MqttAndroidClient client;
+    String topic = "teste/tully";
 
     public MyMqttClient(Context context) {
         client = new MqttAndroidClient(context, "tcp://broker.emqx.io:1883",
                 "teste-tully");
+        connectToMqtt(new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+            }
 
-        connectToMqtt();
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+            }
+        });
     }
 
-    void connectToMqtt() {
+    public void connectToMqtt(IMqttActionListener iMqttActionListener) {
         try {
             IMqttToken token = client.connect();
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     // We are connected
-                    subscribeToMqttChannel();
+                    System.out.println("Conexao realizada com sucesso");
+                    iMqttActionListener.onSuccess(asyncActionToken);
+//                    subscribeToMqttChannel();
                 }
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
                     // Something went wrong e.g. connection timeout or firewall problems
                     System.out.println("fail" + exception.getMessage());
+                    iMqttActionListener.onFailure(asyncActionToken, exception);
                 }
             });
         } catch (MqttException e) {
@@ -41,14 +53,15 @@ public class MyMqttClient {
         }
     }
 
-    IMqttMessageListener iMqttMessageListener = new IMqttMessageListener() {
-        @Override
-        public void messageArrived(String topic, MqttMessage message) throws Exception {
-            System.out.println("message received" + message.toString());
-        }
-    };
+//    IMqttMessageListener iMqttMessageListener = new IMqttMessageListener() {
+//        @Override
+//        public void messageArrived(String topic, MqttMessage message) throws Exception {
+//            System.out.println("message received" + message.toString());
+//
+//        }
+//    };
 
-    void subscribeToMqttChannel() {
+    public void subscribeToMqttChannel(IMqttMessageListener iMqttMessageListener) {
         String topic = "teste/tully";
         int qos = 1;
         try {
@@ -74,23 +87,28 @@ public class MyMqttClient {
         }
     }
 
-    public void sendMessage() {
-//        client.connectWith()
-//                .willPublish()
-//                .topic("italo/messages/1")
-//                .payload("jihad safado".getBytes())
-//                .qos(MqttQos.EXACTLY_ONCE)
-//                .retain(true)
-//                .applyWillPublish()
-//                .send()
-//                .whenComplete((connAck, throwable) -> {
-//                    if (throwable != null) {
-//                        // Handle failure to publish
-//                        System.out.println("Deu ruim") ;
-//                    } else {
-//                        System.out.println("concluiu uhuu") ;
-//                        // Handle successful publish, e.g. logging or incrementing a metric
-//                    }
-//                });
+    public void sendMessage(String message) {
+
+        try {
+            int qos = 1;
+            boolean retained = false;
+            IMqttDeliveryToken subToken = client.publish(topic, new MqttMessage(message.getBytes()));
+
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // The message was published
+                    System.out.println("mensagem enviada com sucesso " + asyncActionToken.getResponse().toString());
+                }
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    System.out.println(exception);
+                }
+            });
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
